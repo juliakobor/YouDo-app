2677dbc66a591d03d27b1fadaf6906362054ab66
Kóbor Júlia <Kbor Jlia>
2021-02-06 11:19:33 +0100

    MZ-02

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,23 @@
-console.log("Console log works!");
+let taskInputForm = document.getElementById("taskForm");
+
+//Új elem hozzáadása
+taskInputForm.addEventListener("submit", event => {
+    event.preventDefault();
+
+    //Feliratok változása
+    document.querySelector("h1").innerText="My first YouDo list";
+    document.querySelector("h2").style.display="none";
+
+    let todoItems = [],
+        todoList = document.createElement('ul');
+    todoItems.push(taskInputForm.item.value);
+    document.getElementsByTagName('body')[0].appendChild(todoList);
+
+    for (i = 0; i < todoItems.length; ++i) {
+        listItem = document.createElement('li');
+        listItem.innerHTML = todoItems[i];
+        todoList.appendChild(listItem);
+    }
+    });
+
+
